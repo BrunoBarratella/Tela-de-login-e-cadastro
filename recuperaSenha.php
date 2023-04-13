a9ea165db57e856705090abf9c963c2d8ff82eda
Bruno Barratella <104685289+BrunoBarratella@users.noreply.github.com>
2023-04-13 07:52:58 -0300

    Update recuperaSenha.php

--- recuperaSenha.php
+++ recuperaSenha.php
@@ -15,12 +15,13 @@ $recebe = $conectado->get();
 <?php
 //Crio a condição onde se o $_GET['email'] estiver preenchido ele cumpre as condições abaixo
 if ($_GET['email']) {
-  //Crio um variavel onde atribuo a ela um array_search, que procura o conteudo do $_GET na minha variavel recebe com a chave com o valor de email_usuario
+  //Crio um variavel onde atribuo a ela um array_search, que procura o conteudo do $_GET na minha variavel $recebe com a chave com o valor de email_usuario
   $retorno = array_search($_GET['email'], array_column($recebe, 'email_usuario'));
 
-  //se a variavel retornar vazio digo que não existe aquele email, e caso contrario se retornar alguma coisa executa tudo abaixo
+  //se a variavel retornar vazio digo que não existe aquele email 
   if ($retorno == '') {
     echo "Parece que este email não foi cadastrado em nenhuma conta";
+    //caso contrario se retornar alguma coisa executa tudo abaixo
   } else {
     echo "<form action='' method='post'>
 <label for='novaSenha'>Digite sua nova senha: </label><br>
@@ -32,8 +33,11 @@ if ($_GET['email']) {
 <button type='submit'>Pronto</button>
 </form>";
 
+    //Condição onde verifico se houve o post da mundaça de senha acima
     if ($_POST) {
+      //se sim faço outra verificação para ver se a $novaSenha e o $repeteSenha são iguais
       if ($_POST['novaSenha'] === $_POST['repeteSenha']) {
+        //se forem iguais então eu atribuo o valor dos  inputs à variavel da classe e chamo a minha função put para atualizar a
         $conectado->senha_usuario = $_POST['repeteSenha'];
         $conectado->email_usuario = $_GET['email'];
 
